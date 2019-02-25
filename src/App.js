502b5a96e6bb9783065d0cd1405f8908058b5ae5
Nataliia Bilan <natali_bilan@ukr.net>
2019-02-25 09:24:02 +0200

    added slider

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux';
 import store from './lib/redux';
 
 import InboxScreen from './components/InboxScreen';
+import ImageSlider from './components/ImageSlider';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <InboxScreen />
+        <ImageSlider />
       </Provider>
     );
   }
--- src/components/ImageSlider.stories.js
+++ src/components/ImageSlider.stories.js
@@ -1,9 +1,39 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
 
-import ImageSlider from './ImageSlider/ImageSlider';
+import ImageSlider from './ImageSlider';
+
+
+const images =
+    [
+        {
+            id: 1,
+            name: "item_1",
+            image: "https://images.asos-media.com/products/paperchase-highlighters/11630174-3?$XXL$&wid=513&fit=constrain",
+        },
+        {
+            id: 2,
+            name: "item_2",
+            image: "https://images.asos-media.com/products/paperchase-highlighters/11630174-4?$XXL$&wid=513&fit=constrain",
+        },
+        {
+            id: 3,
+            name: "item_3",
+            image: "https://images.asos-media.com/products/paperchase-highlighters/11630174-1-multi?$XXL$&wid=513&fit=constrain",
+        },
+        {
+            id: 4,
+            name: "item_4",
+            image: "https://images.asos-media.com/products/paperchase-highlighters/11630174-2?$XXL$&wid=513&fit=constrain",
+        },
+    ];
 
 storiesOf('ImageSlider', module)
+    .addDecorator(withKnobs)
+    .addDecorator(story => <div style={{ padding: '3rem' }}>{story()}</div>)
     .add('default', () => (
-        <ImageSlider></ImageSlider>
+        <ImageSlider
+            images={images}
+        />
     ));
